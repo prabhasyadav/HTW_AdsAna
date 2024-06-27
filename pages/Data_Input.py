cc8a39cb7656e99e2452f2231754b41d6a2e01c1
Pratistha <pratistha.k428@gmail.com>
2024-06-27 21:35:13 +0200

    Implement Data Correction option.

--- pages/Data_Input.py
+++ pages/Data_Input.py
@@ -7,6 +7,27 @@ from calculations import *
 st.set_page_config(page_title="Data Input")
 
 # theme = st_theme()
+st.markdown("""
+<style>
+	.stTabs [data-baseweb="tab-list"] {
+		background-color: #527a8a;
+        padding: 0px 10px 0px 10px;
+        border-radius: 10px
+    }
+
+    .stTabs [data-baseweb="tab"] p{
+        font-size: 1rem;
+        color: white;
+        font-weight: 700;
+    }
+
+	.stTabs [aria-selected="true"] p{
+        font-size: 1.1rem;
+        font-weight: 700;
+        text-shadow: black 2px 2px 5px;
+	}
+</style>""", unsafe_allow_html=True
+)
 
 st.write("# Data Input")
 
@@ -19,12 +40,13 @@ with tab1:
     K=[]
     n=[]
     c0=0
+    sac0=0
     mA_VL = []
     ci = []
     qi = [] 
     if input_file:
         try:
-            mA_VL, ci, qi, K, n, c0 = read_csv_and_extract_columns(input_file)    
+            mA_VL, ci, qi, K, n, c0, sac0 = read_csv_and_extract_columns(input_file)    
         except Exception as e:
             print("Error in reading file:", e)
             st.error('Invalid File Format')
@@ -34,6 +56,7 @@ with tab1:
     with col1:    
         st.subheader("Initial Concentrations")
         c0 = st.number_input("c0 value", value=c0)
+        sac0 = st.number_input("SAC0", value=sac0)
 
         st.subheader("Adsorption Parameters")
         # adsorbable_num = st.selectbox("Number of Adsorption Components", [1,2,3,4,5,6])
@@ -76,14 +99,32 @@ with tab1:
             st.session_state['q_exp_lst'] = isotherm_df['qi'].to_numpy()
 
             st.session_state['c0'] = c0
+            st.session_state['sac0'] = sac0
 
             st.session_state['c_calc_lst']=[]
             st.session_state['q_calc_lst']=[]
+            
+            correction_btn = st.button("Data Correction")
 
             st.success("Data input successfully.")
         except Exception as e:
-            st.error("Something went wrong while loading the input data. Please try again.")
-        
+            st.error(f"Something went wrong while loading the input data. Please try again. {e}")
+
+    if 'dosage_lst' in st.session_state.keys():
+        with st.popover("Data Correction"):
+            a0, a1, r, fig = data_correction(st.session_state['q_exp_lst'], st.session_state['c_exp_lst'])
+            st.markdown(f"""
+                <h5>a0: {a0}</h5>
+                <h5>a1: {a1}</h5>
+                <h5>r: {r}</h5>
+            """, unsafe_allow_html=True)
+            st.pyplot(fig)
+            apply_corr_btn = st.button("Apply Correction")
+            if apply_corr_btn:
+                corr_c_exp_lst = a0 + a1 * st.session_state['q_exp_lst']
+                corr_c0 = a0 + a1 * st.session_state['sac0']
+                st.session_state['corr_c_exp_lst'] = corr_c_exp_lst
+                st.session_state['corr_c0'] = corr_c0
 
 
 
@@ -95,22 +136,26 @@ with tab2:
         
         with col1:
             st.subheader("Isotherm Data")
-            iso_input_df = pd.DataFrame({"mA/VL":st.session_state['dosage_lst'], "Concentration":st.session_state['c_exp_lst'], "Adsorption":st.session_state['q_exp_lst']})
+            if 'corr_c_exp_lst' in st.session_state.keys():
+                iso_input_df = pd.DataFrame({"mA/VL":st.session_state['dosage_lst'], "DOC":st.session_state['c_exp_lst'], "SAC":st.session_state['q_exp_lst'], "Corrected DOC": st.session_state['corr_c_exp_lst']})
+            else:
+                iso_input_df = pd.DataFrame({"mA/VL":st.session_state['dosage_lst'], "DOC":st.session_state['c_exp_lst'], "SAC":st.session_state['q_exp_lst']})
             st.dataframe(iso_input_df, use_container_width=True)
         with col2:
-            col2_1, col2_2 = st.columns(2, gap="small")
-            with col2_1:
-                st.subheader('c0 Value : ')#
-            with col2_2:
-                st.html('''<style>
+            st.html('''<style>
                     p.value_text{
-                            padding-top: 0.5rem;
+                            padding-top: 0.25rem;
                             font-size: 1.5rem;
                             color: white;
                     }
                     </style>''')
+            st.markdown(f'''
+                <p class="value_text">c0 Value : {round(st.session_state['c0'], 2)}</p>''', unsafe_allow_html=True)
+            st.markdown(f'''
+                <p class="value_text">SAC0 Value : {round(st.session_state['sac0'], 2)}</p>''', unsafe_allow_html=True)
+            if 'corr_c0' in st.session_state.keys():
                 st.markdown(f'''
-                    <p class="value_text">{st.session_state['c0']}</p>''', unsafe_allow_html=True)
+                    <p class="value_text">Corrected c0 : {round(st.session_state['corr_c0'], 2)}</p>''', unsafe_allow_html=True)
             st.divider()
             st.subheader("Adsorption Components")
             ads_input_df = pd.DataFrame({"K": st.session_state['K'], "n":st.session_state['n']})
@@ -118,28 +163,6 @@ with tab2:
 
 
 
-
-st.markdown("""
-<style>
-	.stTabs [data-baseweb="tab-list"] {
-		background-color: #527a8a;
-        padding: 0px 10px 0px 10px;
-        border-radius: 10px
-    }
-
-    .stTabs [data-baseweb="tab"] p{
-        font-size: 1rem;
-        color: white;
-        font-weight: 700;
-    }
-
-	.stTabs [aria-selected="true"] p{
-        font-size: 1.1rem;
-        font-weight: 700;
-        text-shadow: black 2px 2px 5px;
-	}
-</style>""", unsafe_allow_html=True
-)
 
 #     st.markdown("""
 # <style>
--- process_data.py
+++ process_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
 
 def read_csv_and_extract_columns(filename):
     # Read the CSV file into a pandas DataFrame
@@ -18,11 +20,21 @@ def read_csv_and_extract_columns(filename):
 
     # Extract c0 as a float variable from the first row
     c0 = float(df['c0'][0])
+    sac0 = float(df['sac0'][0])
 
-    return mA_VL, ci, qi, K, n, c0
+    return mA_VL, ci, qi, K, n, c0, sac0
 
 # def display_input_data(input_file):
 #     data = np.loadtxt(input_file, delimiter=',', skiprows=1)
 #     input_df = pd.DataFrame(data=data[0:,0:],    # values
 #         columns=("mA/VL", "c(DOC)", "q_exp"))
 #     return data, input_df
+
+def data_correction(x, y):
+    result = stats.linregress(x, y)
+    fig, ax = plt.subplots(figsize=(10, 6))
+            
+    ax.plot(x, y, 'o', label='original_plot')
+    ax.plot(x, result.intercept + result.slope*x, 'r', label='fitted line')
+    ax.legend()
+    return result.intercept, result.slope, result.rvalue, fig
