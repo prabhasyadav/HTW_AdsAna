14947e39b2d9fcd1f830a537d64fe07423564b58
Pratistha <pratistha.k428@gmail.com>
2024-07-01 23:23:05 +0200

    Implement download input as csv.

--- process_data.py
+++ process_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
+import streamlit as st
 
 def read_csv_and_extract_columns(filename):
     # Read the CSV file into a pandas DataFrame
@@ -24,12 +25,6 @@ def read_csv_and_extract_columns(filename):
 
     return mA_VL, ci, qi, K, n, c0, sac0
 
-# def display_input_data(input_file):
-#     data = np.loadtxt(input_file, delimiter=',', skiprows=1)
-#     input_df = pd.DataFrame(data=data[0:,0:],    # values
-#         columns=("mA/VL", "c(DOC)", "q_exp"))
-#     return data, input_df
-
 def data_correction(x, y):
     result = stats.linregress(x, y)
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -38,3 +33,14 @@ def data_correction(x, y):
     ax.plot(x, result.intercept + result.slope*x, 'r', label='fitted line')
     ax.legend()
     return result.intercept, result.slope, result.rvalue, fig
+
+def download_input_csv():
+    dosage_lst = st.session_state['dosage_lst']
+    c_exp_lst = st.session_state['c_exp_lst']
+    q_exp_lst = st.session_state['q_exp_lst']
+    k = st.session_state['K']
+    n = st.session_state['n']
+    c0 = st.session_state['c0']
+    sac0 = st.session_state['sac0']
+    input_data_df = pd.DataFrame({'mA/VL':pd.Series(dosage_lst), 'ci':pd.Series(c_exp_lst), 'qi':pd.Series(q_exp_lst), 'K':pd.Series(k), 'n':pd.Series(n), 'c0':pd.Series(c0), 'sac0':pd.Series(sac0)})
+    return input_data_df.to_csv(index=False).encode('utf-8')
